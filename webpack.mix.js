01bdb1bc6e8e52e493de8d7e40a5c859fac6c063
Duc Lux <nguyenhuuduc@jv-it.com.vn>
2022-02-19 16:34:32 +0700

    re init

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix')
+const path = require('path')
+require('vuetifyjs-mix-extension')
 
 /*
  |--------------------------------------------------------------------------
@@ -6,11 +8,30 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel applications. By default, we are compiling the CSS
  | file for the application as well as bundling up all the JS files.
  |
  */
+const webpack = {
+  resolve: {
+    alias: {
+      '@resources': path.resolve(__dirname, 'resources/'),
+      '@': path.resolve(__dirname, 'resources/js/src/'),
+      'apexcharts': path.resolve(__dirname, 'node_modules/apexcharts-clevision'),
+    },
+  },
+}
 
-mix.js('resources/js/app.js', 'public/js')
-    .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+mix.webpackConfig(webpack)
+
+mix
+  .js('resources/js/app.js', 'public/js')
+  .vuetify('vuetify-loader', './resources/sass/styles/variables.scss')
+  .vue()
+  .copyDirectory('resources/js/src/assets/images', 'public/images')
+
+mix.webpackConfig({
+  output: {
+    chunkFilename: 'js/chunks/[name].[chunkhash].js',
+  },
+})
